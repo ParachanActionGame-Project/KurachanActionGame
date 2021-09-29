2a604cf64f2aecf0478e81509e25a905a3073062
SantaroHanafusa <undertale1104@gmail.com>
2021-09-29 20:29:34 +0900

    リザルト画面用ぱらちゃんのクラスを仮作成

--- Game/Game/src/ResultScene.hpp
+++ Game/Game/src/ResultScene.hpp
@@ -1,14 +1,14 @@
 ﻿
 # pragma once
 # include "Common.hpp"
-# include "ParachanSample.hpp"
+# include "ResultParachan.hpp"
 
 // リザルトシーン
 // ぱらちゃんを1匹泳がせる。等速直線運動で、スクリーン端で跳ね返る
 class ResultScene : public MyApp::Scene
 {
 private:
-	ParachanSample parachan = ParachanSample(Scene::Center(), 40.0);
+	ResultParachan parachan = ResultParachan(Scene::Center(), 40.0);
 
 	Rect m_startButton = Rect(Arg::center = Scene::Center().movedBy(0, 80), 300, 60);
 	Transition m_startTransition = Transition(0.4s, 0.2s);
